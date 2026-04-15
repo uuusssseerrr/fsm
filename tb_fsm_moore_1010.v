0786da944cfa352490de53b5f0731ad38d2a8db5
Yujeong <87570758+uuusssseerrr@users.noreply.github.com>
2026-04-15 12:59:00 +0900

    Create tb_fsm_moore_1010.v

---
+++ tb_fsm_moore_1010.v
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+
+module tb_fsm_moore_1010();
+
+    reg clk;
+    reg rst;
+    reg din_bit;
+    wire dout_bit;
+
+    fsm_moore_1010 dut (
+    .clk(clk),
+    .rst(rst),
+    .din_bit(din_bit),
+    .dout_bit(dout_bit)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 0;
+        rst = 1;
+        din_bit = 0;
+
+        #20;
+        rst = 0;
+        din_bit = 1; // a->b
+        @(posedge clk); #1;
+
+        din_bit = 0; // b->c
+        @(posedge clk);
+        
+        din_bit = 0; // c->a
+        @(posedge clk);
+        
+        din_bit = 1; //a->b
+        @(posedge clk);
+        
+        din_bit = 0; // b->c
+        @(posedge clk);
+
+        din_bit = 1; //c->d
+        @(posedge clk);
+        
+        din_bit = 0; // d->e, out = 1
+        @(posedge clk);
+
+        #100;
+        $finish;
+    end
+
+endmodule
+
